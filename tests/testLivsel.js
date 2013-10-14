30ecc0b8ed5d632ee841a3a14d36e7c85c2e5b30
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2013-10-14 10:15:33 +0200

    added default test

---
+++ tests/testLivsel.js
@@ -0,0 +1,15 @@
+TestCase("testLivSel", sinon.testCase({
+	setUp: function() 
+	{		
+	},
+	
+	tearDown: function()
+	{
+	},
+
+	// LivSel constructor should be defined.
+	testLivSel: sinon.test(function() {
+		assertNotUndefined(LivSel);
+	})
+
+}));
